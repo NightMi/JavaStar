41a911c73e635695914f0fc7afe063fd49fb13ba
NightMi <d810507000@gmail.com>
2016-11-16 09:44:30 +0800

    Update StartPrint.java
    新增描述

--- StartPrint.java
+++ StartPrint.java
@@ -1,38 +1,21 @@
 package com.pouchen.rnd.training.basic;
 import java.util.Scanner;
-
-public class StartPrint {
-
-
-
-
+public class StartPrint{
 	public static void main(String[] args)
 	{
 		try{
-			Scanner io = new Scanner(System.in);
-		int c = io.nextInt(); //決定幾層
-	        for(int a =1 ; a <= c ; a++)
-	        {
-	            for(int b = 1 ; b <=c ; b++ ) {
-					if(a>=b) { //比對寫幾個（決定三角的直角是在哪）
-						System.out.print("*");
-					}
-				}
-	                System.out.println();
-	        }
-
-
-
-
-
-
+		Scanner io = new Scanner(System.in); //資料來源由使用者輸入
+		int c = io.nextInt(); //接收使用者輸入的值
+	        for(int a =1 ; a <= c ; a++){ //決定幾層
+	        for(int b = 1 ; b <=c ; b++ ) { //決定每層的 * 數量
+		System.out.print("*");
 		}
-		catch (Exception ex)
-		{
-
+	        System.out.println();//負責分層
+	        }
+		}
+		catch (Exception ex){
 		}
 		finally{
-
 		}
 	}
 
